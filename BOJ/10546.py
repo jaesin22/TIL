08214278d01e1f39babadc31e9518dec3238408e
jaesin22 <jaesin22@naver.com>
2021-09-17 18:00:47 +0900

    [0917] BOJ 10546

---
+++ BOJ/10546.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+part = {}
+
+for _ in range(n):
+    name = input().rstrip()
+    if name in part:
+        part[name] += 1
+    else:
+        part[name] = 1
+for _ in range(n-1):
+    name = input().rstrip()
+    part[name] -= 1
+
+for p in part:
+    if part[p]:
+        print(p)
+        break
