a6f7428eaba99f8483e2b6689593fb19a8e52f58
jaesin22 <jaesin22@naver.com>
2021-10-01 13:34:18 +0900

    [1001] BOJ 1449

--- BOJ/1449.py
+++ BOJ/1449.py
@@ -1,9 +1,18 @@
 import sys
-N, L = map(int, sys.stdin.readline())
-loc = list(int, sys.stdin.readline())
+N, L = map(int, sys.stdin.readline().split())
+loc = list(map(int, sys.stdin.readline().split()))
 
-cnt = 0
+cnt = 1
 loc.sort()
 
-for x in range(N):
-    if 
+start = loc[0] # 출발점
+end = start + L - 0.5
+
+for i in loc:
+    if end > i:
+        continue
+    else:
+        cnt += 1
+        end = i + L - 0.5
+
+print(cnt)
