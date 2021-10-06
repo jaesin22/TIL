efd1ae1200142d06095b2321790fc5777db019f4
jaesin22 <jaesin22@naver.com>
2021-10-06 18:00:35 +0900

    [1006] BOJ 1026.py

--- BOJ/1026.py
+++ BOJ/1026.py
@@ -4,5 +4,11 @@ A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
 def array(N, A, B):
-     cnt = 0
-     A.sort(reverse = True)
+    cnt = 0
+    A.sort(reverse = True)
+    B.sort()
+    for x in range(N):
+        cnt += A[x] * B[x]
+    return cnt
+    
+print(array(N,A,B))
