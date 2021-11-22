f9620fc937747d5d15d2715a685e2066e828398f
jaesin22 <jaesin22@naver.com>
2021-11-22 15:59:24 +0900

    [1122] BOJ 2606

---
+++ BOJ/2606.py
@@ -0,0 +1,23 @@
+from sys import stdin
+n = int(stdin.readline())
+m = int(stdin.readline())
+
+graph = [[]*n for _ in range(n+1)]
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [0] * (n+1)
+cnt = 0
+
+def dfs(start):
+    global cnt
+    visited[start] = 1
+    for i in graph[start]:
+        if visited[i] == 0:
+            dfs(i)
+            cnt += 1
+
+dfs(1)
+print(cnt)
