e3b2cd93ddec19a31e98bebdcd6c966e2be2094f
jaesin22 <jaesin22@naver.com>
2021-12-27 14:31:37 +0900

    [1227] BOJ 3184(DFS)

--- BOJ/3184(복습).py
+++ BOJ/3184(복습).py
@@ -1,48 +1,55 @@
 from collections import deque
- 
-def BFS(x, y):
-    queue = deque()
-    sheep = 0
-    wolf = 0
-    if graph[y][x] == 'o':
-        sheep += 1
-    elif graph[y][x] == 'v':
-        wolf += 1
-    
-    queue.append((x, y))
-    graph[y][x] = '#'
-    while queue:
-        now_x, now_y = queue.popleft()
+import sys
 
-        for i in range(4):
-            nx = now_x + dx[i]
-            ny = now_y + dy[i]
-            if 0 <= nx < C and 0 <= ny < R:
-                if graph[ny][nx] == 'o':
-                    sheep += 1
-                elif graph[ny][nx] == 'v':
-                    wolf += 1
-                queue.append((nx, ny))
-                graph[ny][nx] = '#'
-    if sheep > wolf:
-        res[0] += sheep
-    else:
-        res[1] += wolf
+# 가로 세로 길이
+R, C = map(int, sys.stdin.readline().split())
 
+graph = deque()
+# 방문체크
+visited = [[0] * C for _ in range(R)]
 
-R, C = map(int, input().split())
-graph = []
-res = [0, 0]
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
+# 입력
 for _ in range(R):
-    graph.append(list(map(str, input())))
+    graph.append(list(sys.stdin.readline().rstrip()))
+
+# 상하좌우
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def DFS(x, y):
+    global w, s
+    visited[x][y] = 1
+    # 늑대면 a 1 증가
+    if graph[x][y] == 'v':
+        w += 1
+    elif graph[x][y] == 'k':
+        s += 1
+    
+    # 4방 탐색
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        # 범위 안에 있고, #이 아니고 방문을 하지 않았으면 dfs 돌자
+        if 0 <= nx < R and 0 <= ny < C and graph[nx][ny] != '#' and visited[nx][ny] == 0:
+            DFS(nx, ny)
+    
+
+wolf = 0 # 총 양 마리 수
+sheep = 0 # 총 늑대의 마리 수
 
 for i in range(R):
     for j in range(C):
-        if graph[i][j] != '#':
-            BFS(j, i)
+        if graph[i][j] != '#' and visited[i][j] == 0:
+            w = 0
+            s = 0
+            DFS(i, j)
+            if w >= s :
+                s = 0
+            else :
+                w = 0
+                
+            wolf += w
+            sheep += s
 
-print(*res)
-
+print(sheep, wolf)
