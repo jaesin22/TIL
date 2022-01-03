0314a85a5cf9e6cb1208f89b2efd6c024079bd82
jaesin22 <jaesin22@naver.com>
2022-01-03 15:59:16 +0900

    [0103] BOJ 3187

--- BOJ/3187.py
+++ BOJ/3187.py
@@ -1,47 +1,44 @@
 import sys
-
-R, C = map(int, sys.stdin.readline().split())
-
-graph = []
-visited = [[0] * C for _ in range(R)]
-
-wolf = 0
-sheep = 0
-
-for x in range(R):
-    graph.append(list(sys.stdin.readline().rstrip()))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def dfs(x, y):
-    global w, s
-    visited[x][y] = 1
-
-    if graph[x][y] == 'v':
-        w += 1
-    elif graph[x][y] == 'k':
-        s += 1
+from collections import deque
+input = sys.stdin.readline
+R, C = map(int,input().split())
+m = []
+for _ in range(R):
+    m.append(list(input().rstrip()))
+answer = [0, 0]         # sheep, wolf
+q = deque()
+visited = [[False] * C for _ in range(R)]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+ 
+def bfs(i, j):
+    sheep, wolf = 0, 0
+    if m[i][j] == 'v':
+        wolf = 1
+    if m[i][j] == 'k':
+        sheep = 1
+    q.append((i, j))
+    visited[i][j] = True
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < R and 0 <= ny < C and not visited[nx][ny] and m[nx][ny] != '#':
+                visited[nx][ny] = True
+                if m[nx][ny] == 'v':
+                    wolf += 1
+                elif m[nx][ny] == 'k':
+                    sheep += 1
+                q.append((nx, ny))
     
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if 0 <= nx < R and 0 <= ny < C and graph[nx][ny] != '#' and visited[nx][ny] == 0:
-            dfs(nx, ny)
-
+    if sheep > wolf:
+        answer[0] += sheep
+    else:
+        answer[1] += wolf
+ 
+ 
 for i in range(R):
     for j in range(C):
-        if graph[i][j] != '#' and visited[i][j] == 0:
-            w = 0
-            s = 0
-            dfs(i, j)
-            if w >= s:
-                s = 0
-            else:
-                w = 0
-            
-            wolf += w
-            sheep += s
-
-print(sheep, wolf)
+        if m[i][j] != '#' and not visited[i][j]:
+            bfs(i, j)
+ 
+print(*answer)
