7cb61c7bdfcd1e42b6f9be7b81067cfed704ff3f
jaesin22 <jaesin22@naver.com>
2022-01-21 13:13:22 +0900

    [0121] BOJ 1316

--- BOJ/1316.py
+++ BOJ/1316.py
@@ -1,13 +1,14 @@
 import sys
 
 N = int(sys.stdin.readline())
-
 for _ in range(N):
     word = sys.stdin.readline().rstrip()
 
     for i in range(len(word)-1):
-        if word[i] != word[i+1]:
-            if word[i] in word[i+1:]:
-                N -= 1
+        if word[i] == word[i+1]:
+            pass
+        elif word[i] in word[i+1:]:
+            N -= 1
+            break
 
 print(N)
