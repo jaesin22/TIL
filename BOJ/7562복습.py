b0b70c6218b24b2782185952af22bb864902d3be
jaesin22 <jaesin22@naver.com>
2022-01-21 17:20:28 +0900

    7562 remind

--- BOJ/7562복습.py
+++ BOJ/7562복습.py
@@ -1,21 +1,22 @@
 from collections import deque
 import sys
 
-T = int(input())
-dx = [-2, -2, 2,2, -1,1,-1,1]
-dy = [-1,1,-1,1,-2,-2,2,2]
+input = sys.stdin.readline
+I = int(input())
 
-for i in range(T):
-    I = int(input())
+for _ in range(I):
+    knight = int(input())
     start_x, start_y = map(int, input().split())
     end_x, end_y = map(int, input().split())
-
-    visited = [[0] * I for _ in range(I)]
+    visited = [[0] * knight for _ in range(knight)]
     visited[start_x][start_y] = 1
+
+    dx = [-2, -2, 2,2, -1,1,-1,1]
+    dy = [-1,1,-1,1,-2,-2,2,2]
 
     queue = deque()
     queue.append((start_x, start_y, 0))
-    cnt = 0
+
     while queue:
         x, y, c = queue.popleft()
         if x == end_x and y == end_y:
@@ -26,6 +27,6 @@ for i in range(T):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < I and 0 <= ny < I and visited[nx][ny] == 0:
-                queue.append((nx, ny, (c + 1)))
+            if 0 <= nx < knight and 0 <= ny < knight and visited[nx][ny] == 0:
                 visited[nx][ny] = 1
+                queue.append((nx, ny, (c+1)))
