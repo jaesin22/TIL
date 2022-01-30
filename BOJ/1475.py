ed7ecf558d8a8cc24d039b6e8ed1c95ad9e5599e
Jaesin22 <jaesin22@naver.com>
2022-01-30 14:53:57 +0900

    [0130] BOJ 1475

--- BOJ/1475.py
+++ BOJ/1475.py
@@ -1,21 +1,19 @@
-import sys
-import math
+arr = [0] * 10
+cnt = 0
+n = input()
 
+for i in n:
+    arr[int(i)] += 1
 
-def Room_number(N):
-    dasom = [0] * 9
+six_nine = arr.pop(6) + arr.pop(8)  # 6과 9가 필요한 갯수를 추출하고 삭제
 
-    for n in N:
-        if any((n == 6, n == 9)):
-            dasom[5] += 0.5
-        else:
-            dasom[n] += 1
+if six_nine % 2 == 0:
+    six_nine = six_nine//2  # 6과 9를 통틀어 필요한 개수를 계산
+else:
+    six_nine = six_nine//2+1
 
-    answer = math.ceil(max(dasom))
-
-    return answer
-
-
-N = list(map(int, str(input())))
-
-print(Room_number(N))
+if six_nine >= max(arr):
+    print(six_nine)
+# 6,9와 나머지 수 중 제일 많이 카운팅된것 비교하여 출력
+else:
+    print(max(arr))
