94b7633e1e3e482bdbd9db1a304a6849c475fe1a
Jaesin22 <jaesin22@naver.com>
2022-11-01 22:09:45 +0900

    boj 7562

--- BOJ/7562복습.py
+++ BOJ/7562복습.py
@@ -1,32 +1,41 @@
-from collections import deque
 import sys
-
+from collections import deque
 input = sys.stdin.readline
-I = int(input())
 
-for _ in range(I):
-    knight = int(input())
-    start_x, start_y = map(int, input().split())
-    end_x, end_y = map(int, input().split())
-    visited = [[0] * knight for _ in range(knight)]
-    visited[start_x][start_y] = 1
 
-    dx = [-2, -2, 2,2, -1,1,-1,1]
-    dy = [-1,1,-1,1,-2,-2,2,2]
+dx = [-2, -2, 2,2, -1,1,-1,1]
+dy = [-1,1,-1,1,-2,-2,2,2]
 
+def bfs(start_x,start_y, end_x, end_y):
     queue = deque()
     queue.append((start_x, start_y, 0))
+    visited[start_x][start_y] = 1
 
     while queue:
-        x, y, c = queue.popleft()
-        if x == end_x and y == end_y:
+        a, b, c = queue.popleft()
+        if a == end_x and b == end_y:
             print(c)
-            break
-
+            return
+        
         for i in range(8):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < knight and 0 <= ny < knight and visited[nx][ny] == 0:
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if 0 <= nx < I and 0 <= ny < I and visited[nx][ny] == 0:
+                queue.append((nx, ny, c + 1))
                 visited[nx][ny] = 1
-                queue.append((nx, ny, (c+1)))
+                
+
+
+
+T = int(input())
+for x in range(T):
+    I = int(input())
+    visited = [[0] * I for _ in range(I)]
+    start_x, start_y = map(int, input().split())
+    end_x, end_y = map(int, input().split())
+
+    if start_x == end_x and start_y == end_y:
+        print(0)
+        continue
+
+    bfs(start_x, start_y, end_x, end_y)
