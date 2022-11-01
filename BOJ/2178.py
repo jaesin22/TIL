d17b220d619d7a68ad8fcef72d53a285f5d2f2ea
Jaesin22 <jaesin22@naver.com>
2022-11-01 22:43:28 +0900

    [2022-11-01] boj 2178.py

--- BOJ/2178.py
+++ BOJ/2178.py
@@ -1,19 +1,32 @@
-n, m = map(int, input().split())
-s = []
-queue = []
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-for i in range(n):
-    s.append(list(input()))
-queue = [[0, 0]]
-s[0][0] = 1
-while queue:
-    a, b = queue[0][0], queue[0][1]
-    del queue[0]
-    for i in range(4):
-        x = a + dx[i]
-        y = b + dy[i]
-        if 0 <= x < n and 0 <= y < m and s[x][y] == "1":
-            queue.append([x, y])
-            s[x][y] = s[a][b] + 1
-print(s[n - 1][m - 1])
+import sys
+from collections import deque
+input = sys.stdin.readline
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+N, M = map(int, input().split())
+graph = []
+for x in range(N):
+    graph.append(list(map(str, input().rstrip())))
+visited = [[False] * M for _ in range(N)]
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y, 1))
+    visited[x][y] = True
+    graph[x][y] = 0
+    while queue:
+        a, b, c = queue.popleft()
+        if a == N-1 and b == M-1:
+            print(c)
+            break
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == False and graph[nx][ny] == '1':
+                visited[nx][ny] = True
+                graph[nx][ny] = 0
+                queue.append((nx, ny, c+1))
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == '1' and visited[i][j] == False:
+            bfs(i, j)
