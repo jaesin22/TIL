d998eb6b9f71d7c9279e84e91adcd8dffd5793d9
Jaesin22 <jaesin22@naver.com>
2022-11-26 20:25:27 +0900

    [2021] 16946 해결 못함 ㅠ

--- BOJ/11728.py
+++ BOJ/11728.py
@@ -1,16 +1,29 @@
+from re import A
 import sys
-
 input = sys.stdin.readline
-
 
 N, M = map(int, input().split())
 
+arr_a = list(map(int, input().split()))
+arr_b = list(map(int, input().split()))
+res = []
+a_index, b_index = 0,0
 
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
 
-res = A + B
-res.sort()
+while  a_index < N and b_index < M:
+    if arr_a[a_index] <= arr_b[b_index]:
+        res.append(arr_a[a_index])
+        a_index += 1
+    else:
+        res.append(arr_b[b_index])
+        b_index += 1
 
-for x in res:
-    print(x, end=' ')
+while a_index < len(arr_a):
+    res.append(arr_a[a_index])
+    a_index += 1
+
+while b_index < len(arr_b):
+    res.append(arr_b[b_index])
+    b_index += 1
+
+print(*res)
