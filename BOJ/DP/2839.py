a6b70ef941fb5ca858e169500ad771fbef06d651
DESKTOP-8AGN2O9\TI14 <hyeonjae.kim@telusinternational.com>
2022-12-07 12:18:19 +0900

    [2022-12-07] BOJ 2839

---
+++ BOJ/DP/2839.py
@@ -0,0 +1,13 @@
+INF = int(1e9)
+n = int(input())
+
+d = [INF] * 100
+d[3] = 1
+d[5] = 1
+for i in range(3, n+1):
+    d[i] = min(d[i], d[i-3] + 1, d[i-5]+1)
+
+if d[n] == INF:
+    print(-1)
+else:
+    print(d)
