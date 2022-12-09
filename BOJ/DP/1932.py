75b3f9e43ec74be017083992c928d46858e6e354
DESKTOP-8AGN2O9\TI14 <hyeonjae.kim@telusinternational.com>
2022-12-09 12:06:59 +0900

    [2022-12-09] BOJ 1932 더 나은 코드

--- BOJ/DP/1932.py
+++ BOJ/DP/1932.py
@@ -8,12 +8,14 @@ for x in range(N):
 
 k = 2
 for i in range(1, N):
-    for j in range(k):
-        if j == 0:
-            dp[i][j] = dp[i][j] + dp[i-1][j]
-        elif j == i:
+    for j in range(i+1): # i+1을 하는 이유는 i번째줄보다 j번째 줄이 하나 더 많기 때문
+        if j == 0: #왼쪽 가장자리
+            dp[i][j] = dp[i-1][j] + dp[i][j]
+        
+        elif j == i: # 오른쪽 끝
             dp[i][j] = dp[i][j] + dp[i-1][j-1]
-        else:
+        
+        else: # 그외에 중간
             dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + dp[i][j]
         
     k += 1
