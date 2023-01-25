b52a867c46aa38901a90c9ff1dc1e9776617f54f
HyeonJae-Kim <hyeonjae.kim@telusinternational.com>
2023-01-25 16:42:16 +0900

    [2022-01-25] BOJ 2247

--- BOJ/2447.py
+++ BOJ/2447.py
@@ -1,20 +1,39 @@
 import sys
-sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
-def append_star(LEN):
-    if LEN == 1:
-        return ['*']
 
-    Stars = append_star(LEN//3) 
-    L = []  
+
+def divstar(graph, x, y, n):
+    temp = n // 3
+    star(graph, x, y, temp)
+    star(graph, x, y + temp, temp)
+    star(graph, x, y + temp * 2, temp)
+    star(graph, x + temp, y, temp)
+    blank(graph, x + temp, y + temp, temp)
+    star(graph, x + temp, y + temp * 2, temp)
+    star(graph, x + temp * 2, y, temp)
+    star(graph, x + temp * 2, y + temp, temp)
+    star(graph, x + temp * 2, y + temp * 2, temp)
+
+
+def blank(graph, x, y, n):
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            graph[i][j] = ' '
+
+def star(graph, x, y, n):
+    if n == 1:
+        return
     
-    for S in Stars:
-        L.append(S*3)
-    for S in Stars:
-        L.append(S+' '*(LEN//3)+S)
-    for S in Stars:
-        L.append(S*3)
-    return L
+    divstar(graph, x, y, n)
 
-n = int(sys.stdin.readline().strip())
-print('\n'.join(append_star(n)))
+
+N = int(input())
+graph = [['*'] * N for _ in range(N)]
+
+divstar(graph, 0, 0, N)
+
+for i in range(N):
+    for j in range(N):
+        print(graph[i][j], end='')
+    print()
