e231044ecbc145b5433f383fc24d7711c0fa59db
HyeonJae-Kim <hyeonjae.kim@telusinternational.com>
2023-01-30 11:04:23 +0900

    [2023-01-30] BOJ 2156_remind

--- BOJ/DP/2156.py
+++ BOJ/DP/2156.py
@@ -2,17 +2,19 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-array = [int(input()) for _ in range(N)]
+arr = []
+for i in range(N):
+    arr.append(int(input()))
+
 if N == 1 or N == 2:
-    print(sum(array))
+    print(sum(arr))
+    sys.exit()
 
-else:
-    dp = [0] * (N+1)
-    dp[0] = array[0] # 6
-    dp[1] = array[0] + array[1] # 6 + 10 = 16
+dp = [0] * (N + 1)
+dp[0] = arr[0]
+dp[1] = arr[0] + arr[1]
 
-    for i in range(2, N):
-        dp[i] = max(dp[i-1], dp[i-2] + array[i], dp[i-3] + array[i-1] + array[i])
-    
-    print(dp[N-1])
+for i in range(2, N):
+    dp[i] = max(dp[i-1], dp[i-2] + arr[i], dp[i-3] + arr[i] + arr[i-1])
 
+print(dp[N-1])
