902163827a23006d6db14dc1fc8706d9244a7fcd
HyeonJae-Kim <hyeonjae.kim@telusinternational.com>
2023-02-16 16:34:47 +0900

    [2023-02-16] BOJ 15657

---
+++ BOJ/Back Tracking/15656.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+A.sort()
+arr = []
+visited = [0] * N
+
+def solved(start):
+    if len(arr) == M:
+        print(' '.join(map(str, arr)))
+        return
+
+    for i in range(N):
+        arr.append(A[i])
+        solved(i)
+        arr.pop()
+
+solved(0)
