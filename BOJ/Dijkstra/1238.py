06f10fd43e158417cb2af7cb26d3cda0cce03731
HyeonJae-Kim <hyeonjae.kim@telusinternational.com>
2023-02-27 12:14:21 +0900

    [2023-02-27] BOJ 1238

--- BOJ/Dijkstra/1238.py
+++ BOJ/Dijkstra/1238.py
@@ -4,19 +4,37 @@ input = sys.stdin.readline
 INF = int(1e9)
 
 N, M, X = map(int, input().split())
-graph = [[] * (N+1)]
-distance = [INF] * (N+1)
-for i in range(M):
-    u, m, v = map(int,input().split())
+graph = [[] for i in range(N + 1)]
+
+for _ in range(M):
+    u, m, v = map(int, input().split())
     graph[u].append((m, v))
 
 def dijkstra(start):
+    distance = [INF] * (N+1)
     queue = []
     heapq.heappush(queue, (0, start))
     distance[start] = 0
 
-for i in range(N):
-    dijkstra(i)
+    while queue:
+        dist, curr = heapq.heappop(queue)
+        
+        if dist > distance[curr]:
+            continue
+        
+        for next, weight in graph[curr]:
+            cost = dist + weight
+            if cost < distance[next]:
+                distance[next] = cost
+                heapq.heappush(queue, (cost, next))
+    
+    return distance
+
+res = 0
+back = dijkstra(X)
+for i in range(1, N+1):
+    go = dijkstra(i)
+    res = max(res, go[X] + back[i]) # go[X] = X 번 마을까지의 최단 경로, back[i] = 나머지 다른 마을로의 모든 최단경로
 
 
-
+print(res)
