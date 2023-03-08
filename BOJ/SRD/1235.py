e556a6fd719252b2ed3c32263641c368c5849f92
HyeonJae-Kim <hyeonjae.kim@telusinternational.com>
2023-03-08 17:56:36 +0900

    [2023-03-08] BOJ 1235

--- BOJ/SRD/1235.py
+++ BOJ/SRD/1235.py
@@ -4,27 +4,16 @@ input = sys.stdin.readline
 N = int(input())
 arr = []
 for i in range(N):
-    arr.append(list(map(int, input().rstrip())))
+    arr.append(str(input().strip()))
 
-
-res = arr.pop(0)
-k = len(res)
-A = []
-cnt = 1
-
-for i in range(len(res) - 1, -1, -1):
-    a = res[i:k]
-    a = ''.join(map(str, a))
-    A.append(a)
-    for j in arr:            
-        s = j[i:k]
-        s = ''.join(map(str, s))
-        if s in A:
-            cnt += 1
+for i in range(1, len(arr[0]) + 1):
+    res = []
+    for j in range(N):
+        if arr[j][-i:] in res:
+            break
         else:
-            A.append(s)
-        
-        if len(A) == N:
-            print(cnt)
-            sys.exit()
-    A = []
+            res.append(arr[j][-i:])
+
+    if len(res) == N:
+        print(i)
+        break
