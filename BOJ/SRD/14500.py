d1e6c4609b49df5d93f316f589056655062775d1
HyeonJae-Kim <hyeonjae.kim@telusinternational.com>
2023-03-29 14:59:13 +0900

    [2023-03-29] BOJ 14500

--- BOJ/SRD/14500.py
+++ BOJ/SRD/14500.py
@@ -1,18 +1,17 @@
 import sys
 input = sys.stdin.readline
- 
+
 N, M = map(int, input().split())
 graph = []
-check = [[0 for _ in range(M)] for _ in range(N)]
-for _ in range(N):
+for i in range(N):
     graph.append(list(map(int, input().split())))
- 
-block = [
+
+blocks = [
     [[0, 0], [0, 1], [0, 2], [0, 3]],
     [[0, 0], [1, 0], [2, 0], [3, 0]],
- 
+
     [[0, 0], [0, 1], [1, 0], [1, 1]],
- 
+
     [[0, 0], [1, 0], [2, 0], [2, 1]],
     [[0, 0], [1, 0], [0, 1], [0, 2]],
     [[0, 0], [0, 1], [1, 1], [2, 1]],
@@ -21,34 +20,35 @@ block = [
     [[0, 0], [0, 1], [0, 2], [1, 2]],
     [[0, 0], [1, 0], [2, 0], [2, -1]],
     [[0, 0], [-1, 0], [0, 1], [0, 2]],
- 
+
     [[0, 0], [1, 0], [1, 1], [2, 1]],
     [[0, 0], [-1, 1], [0, 1], [-1, 2]],
     [[0, 0], [0, 1], [1, 1], [1, 2]],
     [[0, 0], [1, 0], [1, -1], [2, -1]],
- 
-    [[0, 0], [1, -1], [1, 0], [1, 1]],
-    [[0, 0], [1, 0], [1, 1], [2, 0]],
-    [[0, 0], [0, 1], [0, 2], [1, 1]],
-    [[0, 0], [1, 0], [1, -1], [2, 0]]
+    
+    [[0, 0], [0, 1], [1, 1], [0, 2]],
+    [[0, 0], [1, 0], [1, -1], [2, 0]],
+    [[0 ,0], [0, 1], [-1, 1], [0, 2]],
+    [[0, 0], [1, 0], [1, 1], [2, 0]]
 ]
- 
-def solve(x, y):
-    result = 0 # 해당 지점에서 발생할 수 있는 최종값
-    for i in range(len(block)): # 각 블록의 유형에 대해
+
+def cal(x, y):
+    result = 0
+    for i in range(len(blocks)):
         s = 0 # 산출값 초기화
-        v_list = [[x + nx, y + ny] for nx, ny in block[i]]
-        # print(v_list)
-        for v in v_list: # 블록의 각 지점에 대해
-            if 0 <= v[0] < N and 0 <= v[1] < M: # 범위 내라면
-                s += graph[v[0]][v[1]] # 값 반영
-            else: # 범위 밖이면 더 볼 필요 없음
+        v_list = [[x + ax, y + ay] for ax, ay in blocks[i]]
+        
+        for nx, ny in v_list:
+            if 0 <= nx < N and 0 <= ny < M:
+                s += graph[nx][ny]
+            else:
                 break
-        result = max(result, s) # 각 블록에 대해 최대값 점검
-    return result # 최종값 반환
- 
+        result = max(result, s)
+    return result
+
 res = 0
 for i in range(N):
     for j in range(M):
-        res = max(res, solve(i, j))
+        res = max(res, cal(i, j))
+
 print(res)
