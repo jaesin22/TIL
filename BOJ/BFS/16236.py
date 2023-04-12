878aad7960eb266b51e18d95e09a5e06bd4259b1
HyeonJae-Kim <hyeonjae.kim@telusinternational.com>
2023-04-12 17:16:45 +0900

    [2023-04-12] BOJ 16236

--- BOJ/BFS/16236.py
+++ BOJ/BFS/16236.py
@@ -1,67 +1,68 @@
 import sys
-input = sys.stdin.readline
 from collections import deque
-
-
-# 물고기를 먹으러갈 때마다 bfs로 이동한다.
-# bfs로 이동하고 이동한 칸 수 만큼 answer에 더해준다. 
-# 현재 상어의 크기 만큼 물고기를 먹었으면 상어의 크기 + 1해준다. 
-# 틀렸던 부분 ) "먹을 수 있는 물고기가 1마리보다 많다면, 거리가 가장 가까운 물고기 -> 가장 위에 -> 가장 왼쪽에"  
-# : 이 부분 구현은 bfs와 move의 순서 조정으로 할 수 있을 줄 알았는데, 먹을 수 있는 물고기를 다 고려해준 후에 sort를 해줘야지 정확한 답이 나온다.
-
-N = int(input())
-graph = []
-visited = [[0] * N for _ in range(N)]
-# 북, 남, 동, 서
+input = sys.stdin.readline
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
-for _ in range(N):
-    graph.append(list(map(int, input().split())))
+INF = int(1e9)
 
-sx, sy = 0,0
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+baby = 2
+x, y = 0, 0
 for i in range(N):
     for j in range(N):
         if graph[i][j] == 9:
-            sx, sy = i, j
+            x, y = i, j
             graph[i][j] = 0
 
-def bfs(x, y, shark):
+def search():
     queue = deque()
-    queue.append((x, y, 0))
-    visited = [[0] * N for _ in range(N)]
-    eat = []
+    queue.append((x, y))
+    visited = [[-1] * N for _ in range(N)]
+    visited[x][y] = 0
     while queue:
-        a, b, c = queue.popleft()
-        if visited[a][b] != 0:
-            continue
-        visited[a][b] = c
-        if graph[a][b] < shark and graph[a][b] != 0:
-            eat.append((c, a, b))
-            for i in range(4):
-                nx = a + dx[i]
-                ny = b + dy[i]
-                if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] <= shark:
-                    queue.append((nx, ny, c + 1))
-        if not eat:
-            return (-1, -1, -1)
-        eat.sort()
-        graph[eat[0][1]][eat[0][2]] = 0
-        return eat[0]
+        a, b = queue.popleft()
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if 0 <= nx < N and 0 <= ny < N:
+                if baby >= graph[nx][ny] and visited[nx][ny] == -1:
+                    visited[nx][ny] = visited[a][b] + 1
+                    queue.append((nx, ny))
+    
+    return visited 
 
-shark = 2
-answer = 0
 
-isContinue = True
-while isContinue:
-    for i in range(shark):
-        c, x, y = bfs(sx, sy, shark)
-        # print(c)
-        if c == -1:
-            isContinue = False
-            break
-        sx, sy = x, y
-        answer += c
+def check(visited):
+    a, b = 0, 0
+    dist = INF
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] != -1 and 1 <= graph[i][j] < baby:
+                if visited[i][j] < dist:
+                    dist = visited[i][j]
+                    a, b = i, j
+    
+    if dist == INF:
+        return False
     else:
-        shark += 1
+        return a, b, dist
 
-print(answer)
+ans = 0
+cnt = 0
+
+while True:
+    res = check(search())
+
+    if not res:
+        print(ans)
+        break
+    else:
+        x, y = res[0], res[1]
+        ans += res[2]
+        graph[x][y] = 0
+        cnt += 1
+    
+    if cnt >= baby:
+        baby += 1
+        cnt = 0
