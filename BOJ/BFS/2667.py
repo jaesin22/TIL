c839133f6db613826c3c9968c4bf9f898be94511
Jaesin22 <jaesin22@naver.com>
2023-09-07 22:12:26 +0900

    bfs 복습

--- BOJ/BFS/2667.py
+++ BOJ/BFS/2667.py
@@ -1,43 +1,42 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
-def bfs(graph, a, b):
-    n = len(graph)
+
+def bfs(x, y):
     queue = deque()
-    queue.append((a, b))
-    graph[a][b] = 0
+    queue.append((x, y))
     count = 1
-
+    visited[x][y] = 1
     while queue:
-        x, y = queue.popleft()
+        a, b = queue.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = a + dx[i]
+            ny = b + dy[i]
 
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
-                count += 1
+            if 0 <= nx < N and 0 <= ny < N:
+                if graph[nx][ny] == 1 and visited[nx][ny] == 0:
+                    visited[nx][ny] = 1
+                    count += 1
+                    queue.append((nx, ny))
+
     return count
 
 
-dx = [0, 0, 1, -1]
-dy = [-1, 1, 0, 0]
-
 N = int(input())
-graph = []
-
-for x in range(N):
-    graph.append(list(map(int, input())))
-
-cnt = []
+visited = [[0] * N for _ in range(N)]
+graph = [list(map(int, input().rstrip())) for _ in range(N)]
+cnt = 1
+res = []
 for i in range(N):
     for j in range(N):
-        if graph[i][j] == 1:
-            cnt.append(bfs(graph, i, j))
+        if graph[i][j] == 1 and visited[i][j] == 0:
+            res.append(bfs(i, j))
+            cnt += 1
 
-cnt.sort()
-print(len(cnt))
-for x in cnt:
-    print(x)
+print(len(res))
+res.sort()
+for i in res:
+    print(i)
